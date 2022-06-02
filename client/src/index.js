95d67bd829ae313916ce419cbf9ce71a08566c9c
VaddeMadhu <madvadde@publicisgroupe.net>
2022-06-02 21:08:35 +0530

    added env file and netlify folder

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,6 @@ const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- client/src/utils/stripe/stripe.utils.js
+++ client/src/utils/stripe/stripe.utils.js
@@ -1,4 +1,2 @@
 import { loadStripe } from "@stripe/stripe-js";
-export const stripePromise = loadStripe(
-  "pk_test_51L67CMSCQ3fAanEO13ewQAQYhjfWM7LjpmF1pMLU778HeSMZZclpxy9ro36bTmV6YHahXf1FcmcHnltJlTLU4tgB00a0LNAHFn"
-);
+export const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
